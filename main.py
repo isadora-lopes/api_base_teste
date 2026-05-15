a2155f15fe350cb9b7715a5ac8769258ee57b681
isa <isadoracustodiolopes4@gmail.com>
2026-05-15 17:26:52 -0300

    rotas de operadoras e protocolos adicionadas

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
 from database import get_db
+from models import Operadora, Protocolo
 
 app = FastAPI()
 
-@app.get("/")
-def rota():
-    print(get_db())
-    return("rota funcionando :)")
+# rota para listar operadoras
+@app.get("/operadoras")
+def listar_operadoras(db: Session = Depends(get_db)):
+    return db.query(Operadora).all()
+
+# rota para listar protocolos de uma operadora
+@app.get("/operadoras/{operadora_id}/protocolos")
+def listar_protocolos_por_operadora(
+    operadora_id: int,
+    db: Session = Depends(get_db)
+):
+    return (
+        db.query(Protocolo)
+        .filter(Protocolo.operadora_id == operadora_id)
+        .all()
+    )
